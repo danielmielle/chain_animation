3b282a42c61550794e56ad03dcfce97850abefcb
Danielle Angelo Bilong Bia <danielmielle@gmail.com>
2023-08-21 16:15:39 +0800

    update package

--- lib/chain_animation.dart
+++ lib/chain_animation.dart
@@ -6,3 +6,11 @@ export 'src/chain/chain_animation_controller.dart';
 export 'src/chain/chain_animation.dart';
 export 'src/chain/chain_animated_widget.dart';
 export 'src/chain/animate/bounce.dart';
+export 'src/chain/animate/fade_in.dart';
+export 'src/chain/animate/fade_out.dart';
+export 'src/chain/animate/just_invisible.dart';
+export 'src/chain/animate/rotate.dart';
+export 'src/chain/animate/pop_up.dart';
+export 'src/chain/animate/roll.dart';
+
+
